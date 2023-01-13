7c0068e459514853caf2da59c465df2e37d7f998
Shri Ganesh Jha <60005430+TheSGJ@users.noreply.github.com>
2023-01-13 00:16:23 +0000

    New Demo State implementation by React Native Context API

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,14 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import NoteContext from '../context/notes/noteContext';
 
 const About = () => {
   const a = useContext(NoteContext)
+  useEffect(() => {
+    a.update()
+  })
+  
   return (
-    <div>About our leader: {a.name}</div>
+    <div>About our leader: {a.state.name}</div>
   )
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import NoteContext from './noteContext';
+import { useState } from 'react';
 
 const NoteState = (props) =>{
-    const state = {
+    const s1 = {
         "name": "SGJ"
     }
+    const [state, setState] = useState(s1);
+    const update = () =>{
+        setTimeout(()=>{
+            setState({"name": "Mando"})
+        }, 1000)
+    }
     return (
-        <NoteContext.Provider value={state}>
+        <NoteContext.Provider value={{state, update}}>
             {props.children}
         </NoteContext.Provider>
     )
